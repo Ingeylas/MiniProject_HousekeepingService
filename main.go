c200743cd6b1d809c20ef4cf5d0f69fc22eb32cc
Shazia Naveeda <114483889+Ingeylas@users.noreply.github.com>
2024-05-16 18:41:30 +0700

    Update Main.Go

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"rapikan/configs"
 	"rapikan/routes"
+	"os"
 )
 
 func main() {
 	configs.InitDB()
 	e := routes.New()
 
-	e.Start(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	e.Logger.Fatal(e.Start("0.0.0.0:" + port))
 }
